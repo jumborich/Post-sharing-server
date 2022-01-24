5bb591e35e8dc4268ecb7ada8840ace452f4fd81
richiej <jumbor6@gmail.com>
2022-01-24 20:31:38 +0100

    db re-config

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const dbConfig = require("../config/config.json")
+const dbConfig = require("./../config/config.json");
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
