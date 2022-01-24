3cd8c76fd175402871592373faa346917caa01a3
richiej <jumbor6@gmail.com>
2022-01-24 18:07:34 +0100

    Changed bcrypt to bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const { Users } = require("./../models");
 const { ErrorStack } = require("./error");
 
