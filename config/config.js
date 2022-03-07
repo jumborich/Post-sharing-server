5e404837cf07ea39ed5ac677ffbbae3a6940857a
richiej <jumbor6@gmail.com>
2022-03-07 09:32:43 -0500

    masked confidential data in config file

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "Alokube95",
-    "database": "tutorialDB",
+    "password": "*******",
+    "database": "*******",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
